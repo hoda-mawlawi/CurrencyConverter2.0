f7d66d2040e729907fca6fdbae0fb94c81653c0e
Hoda <hoda.hani.mawlawi@gmail.com>
2022-03-27 14:06:23 +0300

    UI of landing page

--- app/src/main/java/com/example/currencyconverter20/Calculator.java
+++ app/src/main/java/com/example/currencyconverter20/Calculator.java
@@ -1,4 +1,13 @@
 package com.example.currencyconverter20;
 
-public class Calculator {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
