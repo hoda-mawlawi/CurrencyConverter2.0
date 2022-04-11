003f6832df8953a99f690d41a5f28c1a78a75ff7
Hoda <hoda.hani.mawlawi@gmail.com>
2022-04-11 15:50:32 +0300

    link between 2 pages (intent)

--- app/src/main/java/com/example/currencyconverter20/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter20/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.currencyconverter20;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void nextPage () {
+        Intent intent = new Intent(getApplicationContext(), Calculator.class);
+        startActivity(intent);
     }
 }
