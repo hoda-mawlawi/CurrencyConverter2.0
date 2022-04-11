96d5fcc9a584ee88b63f7451a0633e29a718fe2e
Hoda <hoda.hani.mawlawi@gmail.com>
2022-04-11 13:50:50 +0300

    add toggle and onlick method of arrow

--- app/src/main/java/com/example/currencyconverter20/Calculator.java
+++ app/src/main/java/com/example/currencyconverter20/Calculator.java
@@ -1,15 +1,59 @@
 package com.example.currencyconverter20;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class Calculator extends AppCompatActivity {
+
+    boolean usd_to_lbp = true;
+    float rate = 24;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get exhange rate
+
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton);
+        ToggleButton toggle2 = (ToggleButton) findViewById(R.id.toggleButton2);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    toggle2.setChecked(false);
+                    //Toast.makeText(getApplicationContext(), "toggle enabled", Toast.LENGTH_LONG).show();
+                } else {
+                    toggle2.setChecked(true);
+                    //Toast.makeText(getApplicationContext(), "toggle disabled", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        toggle2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    toggle.setChecked(false);
+                    //Toast.makeText(getApplicationContext(), "toggle enabled", Toast.LENGTH_LONG).show();
+                } else {
+                    toggle.setChecked(true);
+                    //Toast.makeText(getApplicationContext(), "toggle disabled", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
+    public void convert (View v) {
+        if (usd_to_lbp) {
+
+        } else {
+
+        }
+    }
 
 }
